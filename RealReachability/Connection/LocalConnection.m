a0f0dfa4d39c569f43a6a4b177ddc3b3b18ebb19
凡向阳 <fanxiangyang@keyirobot.com>
2026-04-07 14:29:34 +0800

    fix xcode26.4适配
    #if __clang_major__ < 21 && __has_include(<netinet6/in6.h>)
    #import <netinet6/in6.h>
    #endif

--- RealReachability/Connection/LocalConnection.m
+++ RealReachability/Connection/LocalConnection.m
@@ -8,7 +8,9 @@
 
 #import "LocalConnection.h"
 #import <netinet/in.h>
+#if __clang_major__ < 21 && __has_include(<netinet6/in6.h>)
 #import <netinet6/in6.h>
+#endif
 #import <arpa/inet.h>
 #import <ifaddrs.h>
 
